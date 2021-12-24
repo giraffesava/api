e577b3450dd326f4983158caad026e750bade15e
Sava <savely54@mail.ru>
2021-12-25 01:30:24 +0700

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+// MIDDLEWARES
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+//Routes
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../data/tours-simple.json`)
+);
+
+exports.checkID = (req, res, next, val) => {
+  if (val > tours.length) {
+    return res.status(404).json({
+      status: "failure",
+      message: "invalid id",
+    });
+  }
+  next();
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const tour = tours.find((el) => el.id === req.params.id * 1);
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { ...req.body, newId };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        resulst: tours.length,
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
