8c1eee91cec600ea2227960fe7f527532797a352
Sava <savely54@mail.ru>
2021-12-25 13:36:36 +0700

    added .env

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // MIDDLEWARES
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 //Routes
 app.use("/api/v1/tours", tourRouter);
