daca8491f18fcc7d93b233f4592ad9a1bc415efc
Sava <savely54@mail.ru>
2022-01-10 15:06:10 +0700

    refactoring

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
 dotenv.config({ path: "./config.env" });
+const app = require("./app");
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
-
-const app = require("./app");
 
 mongoose
   .connect(DB, {
