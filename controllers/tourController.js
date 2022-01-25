9620cf3f086e4e6296b2f7cc2047f875a82e7783
Sava <savely54@mail.ru>
2022-01-25 16:15:03 +0700

    added role

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -73,11 +73,13 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndUpdate(req.param.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
   if (!tour) {
     return next(new AppError("No tour found with that ID", 404));
   }
-  res.status(200).json({
+
+  res.status(204).json({
     status: "success",
     data: null,
   });
